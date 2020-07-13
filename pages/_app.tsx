3c28984279bae778635c1bf6f60df06719f69223
Diego Pasquali <imdiegopasquali@gmail.com>
2020-07-13 23:22:49 +0200

    Added base style and resets

---
+++ pages/_app.tsx
@@ -0,0 +1,29 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import 'normalize.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>dgopsq</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+
+        <link
+          href='https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Open+Sans:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
+
+        <style>{`
+          body {
+            font-family: 'Open Sans', sans-serif;
+          }
+        `}</style>
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
