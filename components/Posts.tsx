d2d83451f2854226338d65269254fae38120a14c
Diego Pasquali <imdiegopasquali@gmail.com>
2021-11-09 10:48:10 +0100

    Revert "Renamed `date` to `website_date`"
    This reverts commit 009a0db56f26c7fb8eed07749c1fcf5d352a0839.

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -23,7 +23,7 @@ const Posts: React.FC<Props> = ({ posts }) => {
                   <a>
                     <PostTitle
                       value={post.frontmatter.title}
-                      date={new Date(post.frontmatter.website_date)}
+                      date={new Date(post.frontmatter.date)}
                       tags={post.frontmatter.tags}
                     />
                   </a>
