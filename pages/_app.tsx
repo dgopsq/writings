10089c3690a87def3b351fda0af811d078c6728a
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-07-15 09:23:08 +0200

    Remove unused import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { FunctionComponent } from 'react'
 import Seo from '../components/Seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
