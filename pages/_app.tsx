457660d7d5fc2d8f4038d0fa423876811a87019d
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-07-15 08:56:57 +0200

    Upgrade next to `12` and other minor libs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,20 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { FunctionComponent } from 'react'
 import Seo from '../components/Seo'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const C = Component as FunctionComponent
+
   return (
     <>
       <Head>
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
+
       <Seo />
-      <Component {...pageProps} />
+
+      <C {...pageProps} />
     </>
   )
 }
