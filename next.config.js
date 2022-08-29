a25907b6423c02ace1678e2a2544b519f0ce4ba2
Diego Pasquali <5963683+dgopsq@users.noreply.github.com>
2022-08-29 23:34:36 +0200

    Handle the build phase

--- next.config.js
+++ next.config.js
@@ -6,47 +6,12 @@ module.exports = {
   optimizeFonts: true,
   reactStrictMode: true,
   swcMinify: true,
-  cleanDistDir: false,
 
-  webpack(config, { isServer }) {
+  webpack(config) {
     config.experiments = {
       ...config.experiments,
       asyncWebAssembly: true,
     }
-
-    config.plugins.push(
-      new (class {
-        apply(compiler) {
-          compiler.hooks.afterEmit.tapPromise(
-            'SymlinkWebpackPlugin',
-            async (compiler) => {
-              if (isServer) {
-                const from = path.join(
-                  compiler.options.output.path,
-                  '../static',
-                )
-                const to = path.join(compiler.options.output.path, 'static')
-
-                try {
-                  await fs.accessSync(from)
-                  console.log(`${from} already exists`)
-                  return
-                } catch (error) {
-                  if (error.code === 'ENOENT') {
-                    // No link exists
-                  } else {
-                    throw error
-                  }
-                }
-
-                await fs.symlinkSync(to, from, 'junction')
-                console.log(`created symlink ${from} -> ${to}`)
-              }
-            },
-          )
-        }
-      })(),
-    )
 
     return config
   },
