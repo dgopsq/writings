fcd7f0db8e3adf7031a9c01760ec12ef3c71c9dc
Diego Pasquali <imdiegopasquali@gmail.com>
2023-04-01 23:00:16 +0200

    Add loader

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,10 +1,16 @@
 import dynamic from 'next/dynamic'
+import FadeLoader from 'react-spinners/FadeLoader'
+import { colors } from '../theme'
 
 const ResumeWeb = dynamic(
   () => import('../components/Resume').then(({ ResumeWeb }) => ResumeWeb),
   {
     ssr: false,
-    loading: () => <div>Loading...</div>,
+    loading: () => (
+      <div className='loader-wrapper'>
+        <FadeLoader color={colors.primary} height={13} width={4} margin={1} />
+      </div>
+    ),
   },
 )
 
@@ -18,6 +24,15 @@ export default () => (
         overflow: hidden !important;
         padding: 0 !important;
         margin: 0 !important;
+      }
+
+      .loader-wrapper {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
       }
     `}</style>
   </>
