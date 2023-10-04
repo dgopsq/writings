74968a3058084e1bdce940b6acb9b9f3aec81518
Diego Pasquali <imdiegopasquali@gmail.com>
2023-10-04 19:26:38 +0200

    Add initial styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,17 @@ export const metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className={inter.className}>
-      <body>{children}</body>
+      <body>
+        <div className='mx-auto max-w-4xl'>
+          <div className='mt-12'>
+            <h1 className='font-bold text-3xl uppercase tracking-widest'>
+              Diego Pasquali
+            </h1>
+          </div>
+
+          <div className='mt-6'>{children}</div>
+        </div>
+      </body>
     </html>
   )
 }
