fe86fbb5eec6d7a2af399144b87120387966b6d0
Diego Pasquali <imdiegopasquali@gmail.com>
2023-10-04 20:32:17 +0200

    First blog post style

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: {
+    nested: {},
     tailwindcss: {},
     autoprefixer: {},
   },
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import './style.css'
+
 import { notFound } from 'next/navigation'
 import { getSinglePost } from '../../../lib/posts'
 import { MDXRemote } from 'next-mdx-remote/rsc'
@@ -18,7 +20,7 @@ export default function Page({ params: { slug } }: { params: Params }) {
           {post.frontmatter.title}
         </h2>
 
-        <div className='mt-10'>
+        <div className='post-content mt-10'>
           <MDXRemote source={post.content} />
         </div>
       </div>
